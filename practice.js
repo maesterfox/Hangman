a5385ab339f4a70b495623e72ae8ec023f06a967
maesterfox <talentedmrfox@hotmail.com>
2024-04-19 18:01:23 +0100

    test scripts

---
+++ practice.js
@@ -0,0 +1,25 @@
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) return i;
+  }
+  return -1;
+}
+
+console.log(linearSearch([1, 2, 3, 4, 5], 3)); // 2
+console.log(linearSearch([1, 2, 3, 4, 5], 6)); // -1
+
+// Path: practice.js
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) return mid;
+    if (arr[mid] < target) left = mid + 1;
+    else right = mid - 1;
+  }
+  return -1;
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5], 3)); // 2
+console.log(binarySearch([1, 2, 3, 4, 5], 6)); // -1
