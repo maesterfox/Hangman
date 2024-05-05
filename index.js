151e0a35e32bce1e6248af437976fc592717b5dd
maesterfox <talentedmrfox@hotmail.com>
2024-05-05 19:03:45 +0100

    build and deploy

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url)); // Necessary for
 app.use(cors());
 
 // Since 'dist' is also in the root, you simply refer to it directly
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "src")));
 
 // Handle all GET requests by serving 'index.html' from the 'dist' directory
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "src", "index.html"));
 });
 
 app.listen(PORT, () => {
