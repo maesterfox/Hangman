afcd2abafb6013e9b7ef91f4673fb674a6429a3e
maesterfox <talentedmrfox@hotmail.com>
2024-05-05 19:08:52 +0100

    build and deploy

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, "src")));
 
 // Handle all GET requests by serving 'index.html' from the 'dist' directory
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "src", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
