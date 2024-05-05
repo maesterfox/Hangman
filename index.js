0bb2771088faeaefb1bd3a7e2521b4fa6ba6039a
maesterfox <talentedmrfox@hotmail.com>
2024-05-05 19:01:32 +0100

    build and deploy

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-const PORT = process.env.PORT || 3000; // Use the port assigned by Render or default to 3000
+const PORT = process.env.PORT || 3000;
 
-// Correct way to get __dirname in ES module
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url)); // Necessary for ES Modules to get directory path
 
 app.use(cors());
-// Serve static files from 'dist'
+
+// Since 'dist' is also in the root, you simply refer to it directly
 app.use(express.static(path.join(__dirname, "dist")));
 
-// Handle all GET requests by serving index.html, important for a SPA
+// Handle all GET requests by serving 'index.html' from the 'dist' directory
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://0.0.0.0:${PORT}`); // Bind to all network interfaces
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
